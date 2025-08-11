8f98ac1f77092cf539caab5b8134995bbdc8be10
seun omotayo <omotex_mayorvision@yahoo.com>
2025-08-11 14:15:52 +0100

    deleted

--- files.sh
+++
@@ -1,3 +0,0 @@
-i am tesing this to test 
-I can autheticate here and push
-I am testing to add something in this file then test:
--- script.sh
+++
@@ -1,4 +0,0 @@
- am testing git
-I am tersting SCM
-I am testing Jenkins auto
-
